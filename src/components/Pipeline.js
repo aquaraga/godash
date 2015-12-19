5fa9eb8d44f6956e3216fad6e6711aad88af9a5a
Kumar <aquaraga@gmail.com>
2015-12-20 01:31:04 +0530

    Add first cut of Stage react component

--- src/components/Pipeline.js
+++ src/components/Pipeline.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Stage from './Stage';
 
 export default React.createClass({
   render() {
+  	let details = this.props.data;
+  	let stages = details.instances[0].stages.map(stage => (<Stage data={stage}/>));
     return (
-      <div>
+      <div className='pipeline'>
+        <div className='pipeline-name'>{details.name}</div>
+        <div className='stage-container'>{stages}</div>
       </div>
     );
   }
---
+++ test/components/Pipeline-test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import { expect } from 'chai';
+import Pipeline from '../../src/components/Pipeline';
+import Stage from '../../src/components/Stage';
+
+describe('Pipeline', () => {
+
+	const data = {
+    "name": "Pipeline-1",
+    "instances": [{
+      "stages": [{
+        "name": "Stage-1",
+      }, {
+        "name": "Stage-2"
+      }]
+    }]
+  };
+  const shallowRenderer = TestUtils.createRenderer();
+  shallowRenderer.render(<Pipeline data={data}/>);
+  const pipeline = shallowRenderer.getRenderOutput();
+
+  it('should have a div as container', () => {
+    expect(pipeline.type).to.equal('div');
+    expect(pipeline.props.className).to.equal('pipeline');
+  });
+
+  it('should show the pipeline name', () => {
+    let details = pipeline.props.children;
+    expect(details).to.not.be.undefined;
+    expect(details[0]).to.deep.equal(<div className='pipeline-name'>Pipeline-1</div>);
+
+  });
+
+  it('should show the pipeline details', () => {
+    let details = pipeline.props.children;
+    expect(details[1]).to.deep.equal(
+      <div className='stage-container'>
+        <Stage data={data.instances[0].stages[0]}/>
+        <Stage data={data.instances[0].stages[1]}/>
+      </div>);
+  });
+
+
+});
